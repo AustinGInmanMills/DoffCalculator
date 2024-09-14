5c0bb8bb6618417abfca94f89a85296657f5c791
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-14 06:45:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ machine_1_rpms = 15
 machine_2 = 1600
 machine_2_rpms = 15
 
-machine_3 = 1600
-machine_3_rpms = 15
+machine_3 = 1800
+machine_3_rpms = 10
 
 machine_4 = 1600
-machine_4_rpms = 15
+machine_4_rpms = 10
 
-machine_5 = 1600
-machine_5_rpms = 15
+machine_5 = 1800
+machine_5_rpms = 11
 
 machine_6 = 1600
 machine_6_rpms = 15
@@ -23,11 +23,11 @@ machine_6_rpms = 15
 machine_7 = 1600
 machine_7_rpms = 15
 
-machine_8 = 1600
-machine_8_rpms = 15
+machine_8 = 1400
+machine_8_rpms = 8
 
-machine_9 = 1600
-machine_9_rpms = 15
+machine_9 = 1400
+machine_9_rpms = 10
 
 machine_10 = 1600
 machine_10_rpms = 15
@@ -35,11 +35,11 @@ machine_10_rpms = 15
 machine_11 = 1600
 machine_11_rpms = 15
 
-machine_12 = 1600
-machine_12_rpms = 15
+machine_12 = 2800
+machine_12_rpms = 8
 
-machine_13 = 1600
-machine_13_rpms = 15
+machine_13 = 2800
+machine_13_rpms = 12
 
 machine_14 = 1600
 machine_14_rpms = 15
@@ -60,10 +60,10 @@ machine_19 = 1600
 machine_19_rpms = 15
 
 machine_20 = 1600
-machine_20_rpms = 15
+machine_20_rpms = 8
 
 machine_21 = 1600
-machine_21_rpms = 15
+machine_21_rpms = 8
 
 machine_22 = 1600
 machine_22_rpms = 15

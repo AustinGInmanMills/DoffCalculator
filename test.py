4b8663a51525d5317158116ae2befd0397c2033e
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-14 04:27:27 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+machine_1 = 1600
+machine_1_rpms = 15
+machine_2 = 1600
+machine_2_rpms = 15
+machine_3 = 1600
+machine_3_rpms = 15
+
+st.header("Inman Mills | Doff Calculator [V1.0.0]", divider="rainbow")
+
+st.subheader("Machine #1")
+machine_current_revs = st.text_input("Current Revs", "0", key="1")
+try:
+    machine_current_revs = float(machine_current_revs)
+    calculation = machine_1 - machine_current_revs
+    calculation = calculation / machine_1_rpms
+    if calculation > 60:
+        calculation = calculation / 60
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "hours left")
+    else:
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "minutes")
+except:
+    pass
+
+st.divider()
+
+st.subheader("Machine #2")
+machine_current_revs = st.text_input("Current Revs", "0", key="2")
+try:
+    machine_current_revs = float(machine_current_revs)
+    calculation = machine_2 - machine_current_revs
+    calculation = calculation / machine_2_rpms
+    if calculation > 60:
+        calculation = calculation / 60
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "hours left")
+    else:
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "minutes")
+except:
+    pass
+
+st.divider()
+
+st.subheader("Machine #3")
+machine_current_revs = st.text_input("Current Revs", "0", key="3")
+try:
+    machine_current_revs = float(machine_current_revs)
+    calculation = machine_3 - machine_current_revs
+    calculation = calculation / machine_3_rpms
+    if calculation > 60:
+        calculation = calculation / 60
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "hours left")
+    else:
+        calculation = round(calculation)
+        calculation = str(calculation)
+        st.write(calculation, "minutes")
+except:
+    pass

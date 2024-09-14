ee4eb23dbf5e48eca74162c03cf820b17cc62c47
AustinGInmanMills <xboxboy1387@gmail.com>
2024-09-14 13:20:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,100 @@
 from math import trunc
 import streamlit as st
 
-st.markdown(
-    body="""
-        <style>
-            .block-container{
-                    padding-top: 25px;
-                }
-        </style>
-    """,
-    unsafe_allow_html=True
-)
-
 machine_1 = 1600
 machine_1_rpms = 15
+machine_1_yarn_type = "N/A"
 
 machine_2 = 1600
 machine_2_rpms = 15
+machine_2_yarn_type = "N/A"
 
 machine_3 = 1800
 machine_3_rpms = 10
+machine_3_yarn_type = "Black&White Cone"
 
 machine_4 = 1600
 machine_4_rpms = 10
+machine_4_yarn_type = "Horse Head Cone"
 
 machine_5 = 1800
 machine_5_rpms = 11
+machine_5_yarn_type = "Black&White Cone"
+
 
 machine_6 = 1600
 machine_6_rpms = 15
+machine_6_yarn_type = "Black&White Cone"
 
 machine_7 = 1600
 machine_7_rpms = 15
+machine_7_yarn_type = "Black&White Cone"
 
 machine_8 = 1400
 machine_8_rpms = 8
+machine_8_yarn_type = "Horse Head Cone"
 
 machine_9 = 1400
 machine_9_rpms = 10
+machine_9_yarn_type = "Horse Head Cone"
 
 machine_10 = 1600
 machine_10_rpms = 15
+machine_10_yarn_type = "N/A"
 
 machine_11 = 1600
 machine_11_rpms = 15
+machine_11_yarn_type = "Black&White Cone"
 
 machine_12 = 2800
 machine_12_rpms = 8
+machine_12_yarn_type = "Horse Head Cone"
 
 machine_13 = 2800
 machine_13_rpms = 12
+machine_13_yarn_type = "Horse Head Cone"
 
 machine_14 = 1600
 machine_14_rpms = 15
+machine_14_yarn_type = "N/A"
 
 machine_15 = 1600
 machine_15_rpms = 15
+machine_15_yarn_type = "Black&White Cone"
 
 machine_16 = 1600
 machine_16_rpms = 15
+machine_16_yarn_type = "Black&White Cone"
 
 machine_17 = 1600
 machine_17_rpms = 10
+machine_17_yarn_type = "Pink&Black Cone"
+
 
 machine_18 = 1600
 machine_18_rpms = 8
+machine_18_yarn_type = "Pink&Black Cone"
 
 machine_19 = 1600
 machine_19_rpms = 15
+machine_19_yarn_type = "Black&White Cone"
 
 machine_20 = 1600
 machine_20_rpms = 8
+machine_20_yarn_type = "Pink&Black Cone"
 
 machine_21 = 1600
 machine_21_rpms = 8
+machine_21_yarn_type = "Pink&Black Cone"
 
 machine_22 = 1600
 machine_22_rpms = 15
+machine_22_yarn_type = "Pink&Black Cone"
 
-st.header("Doff Calculator [V1.0.0]", divider="rainbow")
+st.header("Inman Mills | Doff Calculator [V1.0.1]", divider="rainbow")
 
 st.subheader("Machine #1 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_1_yarn_type}]")
 st.write("DOFF = ", int(machine_1))
 st.write("RPMS = ", int(machine_1_rpms))
 machine_current_revs = st.text_input("Current REVS", "0", key="1")
@@ -106,6 +120,7 @@ except:
 st.divider()
 
 st.subheader("Machine #2 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_2_yarn_type}]")
 st.write("DOFF = ", int(machine_2))
 st.write("RPMS = ", int(machine_2_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="2")
@@ -131,6 +146,7 @@ except:
 st.divider()
 
 st.subheader("Machine #3 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_3_yarn_type}]")
 st.write("DOFF = ", int(machine_3))
 st.write("RPMS = ", int(machine_3_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="3")
@@ -156,6 +172,7 @@ except:
 st.divider()
 
 st.subheader("Machine #4 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_4_yarn_type}]")
 st.write("DOFF = ", int(machine_4))
 st.write("RPMS = ", int(machine_4_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="4")
@@ -181,6 +198,7 @@ except:
 st.divider()
 
 st.subheader("Machine #5 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_5_yarn_type}]")
 st.write("DOFF = ", int(machine_5))
 st.write("RPMS = ", int(machine_5_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="5")
@@ -206,6 +224,7 @@ except:
 st.divider()
 
 st.subheader("Machine #6 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_6_yarn_type}]")
 st.write("DOFF = ", int(machine_6))
 st.write("RPMS = ", int(machine_6_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="6")
@@ -231,6 +250,7 @@ except:
 st.divider()
 
 st.subheader("Machine #7 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_7_yarn_type}]")
 st.write("DOFF = ", int(machine_7))
 st.write("RPMS = ", int(machine_7_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="7")
@@ -256,6 +276,7 @@ except:
 st.divider()
 
 st.subheader("Machine #8 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_8_yarn_type}]")
 st.write("DOFF = ", int(machine_8))
 st.write("RPMS = ", int(machine_8_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="8")
@@ -281,6 +302,7 @@ except:
 st.divider()
 
 st.subheader("Machine #9 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_9_yarn_type}]")
 st.write("DOFF = ", int(machine_9))
 st.write("RPMS = ", int(machine_9_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="9")
@@ -306,6 +328,7 @@ except:
 st.divider()
 
 st.subheader("Machine #10 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_10_yarn_type}]")
 st.write("DOFF = ", int(machine_10))
 st.write("RPMS = ", int(machine_10_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="10")
@@ -331,6 +354,7 @@ except:
 st.divider()
 
 st.subheader("Machine #11 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_11_yarn_type}]")
 st.write("DOFF = ", int(machine_11))
 st.write("RPMS = ", int(machine_11_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="11")
@@ -356,6 +380,7 @@ except:
 st.divider()
 
 st.subheader("Machine #12 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_12_yarn_type}]")
 st.write("DOFF = ", int(machine_12))
 st.write("RPMS = ", int(machine_12_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="12")
@@ -381,6 +406,7 @@ except:
 st.divider()
 
 st.subheader("Machine #13 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_13_yarn_type}]")
 st.write("DOFF = ", int(machine_13))
 st.write("RPMS = ", int(machine_13_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="13")
@@ -406,6 +432,7 @@ except:
 st.divider()
 
 st.subheader("Machine #14 | :red[Status Down]")
+st.markdown(f"Yarn Type = :green[{machine_14_yarn_type}]")
 st.write("DOFF = ", int(machine_14))
 st.write("RPMS = ", int(machine_14_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="14")
@@ -431,6 +458,7 @@ except:
 st.divider()
 
 st.subheader("Machine #15 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_15_yarn_type}]")
 st.write("DOFF = ", int(machine_15))
 st.write("RPMS = ", int(machine_15_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="15")
@@ -456,6 +484,7 @@ except:
 st.divider()
 
 st.subheader("Machine #16 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_16_yarn_type}]")
 st.write("DOFF = ", int(machine_16))
 st.write("RPMS = ", int(machine_16_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="16")
@@ -481,6 +510,7 @@ except:
 st.divider()
 
 st.subheader("Machine #17 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_17_yarn_type}]")
 st.write("DOFF = ", int(machine_17))
 st.write("RPMS = ", int(machine_17_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="17")
@@ -506,6 +536,7 @@ except:
 st.divider()
 
 st.subheader("Machine #18 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_18_yarn_type}]")
 st.write("DOFF = ", int(machine_18))
 st.write("RPMS = ", int(machine_18_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="18")
@@ -531,6 +562,7 @@ except:
 st.divider()
 
 st.subheader("Machine #19 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_19_yarn_type}]")
 st.write("DOFF = ", int(machine_19))
 st.write("RPMS = ", int(machine_19_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="19")
@@ -556,6 +588,7 @@ except:
 st.divider()
 
 st.subheader("Machine #20 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_20_yarn_type}]")
 st.write("DOFF = ", int(machine_20))
 st.write("RPMS = ", int(machine_20_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="20")
@@ -581,6 +614,7 @@ except:
 st.divider()
 
 st.subheader("Machine #21 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_21_yarn_type}]")
 st.write("DOFF = ", int(machine_21))
 st.write("RPMS = ", int(machine_21_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="21")
@@ -606,6 +640,7 @@ except:
 st.divider()
 
 st.subheader("Machine #22 | :green[Status Running]")
+st.markdown(f"Yarn Type = :green[{machine_22_yarn_type}]")
 st.write("DOFF = ", int(machine_22))
 st.write("RPMS = ", int(machine_22_rpms))
 machine_current_revs = st.text_input("Current Revs", "0", key="22")

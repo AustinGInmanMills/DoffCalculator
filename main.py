8802df60d0d7e20ab5883b5218fc33dff2f4c7c4
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 05:34:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-revs_input = st.number_input("Current REVS", min_value=0, value="HELLO", step=1, key="rev_input")
+revs_input = st.number_input("Current REVS", min_value=0, value=None, step=1, key="rev_input")
 rpms_input = st.number_input("Current RPMS", min_value=0, step=1, key="rpm_input")
 doff_input = st.number_input("Doff REVS", min_value=0, step=1, key="doff_input")
 calculate_time = st.button("Calculate")

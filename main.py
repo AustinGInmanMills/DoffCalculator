f02591f9114ac0c5da3fc79d0ef97135548881d3
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 05:51:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from math import trunc
 
 revs_input = st.number_input("Current REVS", min_value=0, value=None, step=1, placeholder=None, key="rev_input")
 rpms_input = st.number_input("Current RPMS", min_value=0, value=None, step=1, key="rpm_input")
@@ -6,4 +7,13 @@ doff_input = st.number_input("Doff REVS", min_value=0, value=None, step=1, key="
 calculate_time = st.button("Calculate")
 
 if calculate_time:
-    st.write("HELLO")
+    try:
+        revs_left = float(doff_input) - float(revs_input)
+        time_1_left = revs_left / float(rpms_input)
+        if time_1_left > 60:
+            time_2_left = time_1_left / 60
+            st.write(time_2_left)
+            calculation_1 = trunc(time_2_left)
+            st.write(calculation_1)
+    except:
+        pass

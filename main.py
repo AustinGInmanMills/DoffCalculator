d7843f24281c0a5a429678510ac573c2a7a83eb8
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 05:27:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-revs_input = st.number_input("Current REVS", min_val=0, step=1, key="rev_input")
+revs_input = st.number_input("Current REVS", min_value=0, step=1, key="rev_input")

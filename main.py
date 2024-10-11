611e9d8e6600f8ccbfe00be244a3dfb8ebe8af5a
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 05:25:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
 import streamlit as st
-from math import trunc
 
-revs = st.text_input("Machines Current REVS")
-rpm = st.text_input("Machines Current RPM")
-doff = st.text_input("Machine DOFF REVS")
-revs_input = int(revs)
-rpm_input = int(rpm)
-doff_input = int(doff)
-
-revs_left = doff_input - revs_input
-time_left_1 = revs_left / rpm_input
-calculation = time_left_1
-if time_left_1 > 60:
-	calculation = time_left_1 / 60
-	number_dec1 = trunc(calculation)
-	number_dec2 = str(calculation - int(calculation))[1:]
-	minutes_calc = float(number_dec2) * 60
-	minutes_calc = trunc(minutes_calc)
-	calculation = str(calculation)
-	st.write(str(number_dec1), "hour and", str(minutes_calc), "minutes")
-else:
-	calculation = round(calculation, 2)
-	calculation = str(calculation)
-	st.write(calculation, "minutes")
+revs_input = st.number_input("Current REVS", min_val=0, step=1, key="rev_input")

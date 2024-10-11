1610c9cebb29f49d84682ffaa097a881e6e55593
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 05:59:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,17 @@ doff_input = st.number_input("Doff REVS", min_value=0, value=None, step=1, key="
 calculate_time = st.button("Calculate")
 
 if calculate_time:
-    try:
-        revs_left = float(doff_input) - float(revs_input)
-        time_1_left = revs_left / float(rpms_input)
-        if time_1_left > 60:
-            time_2_left = time_1_left / 60
-            st.write(time_2_left)
-            calculation_1 = trunc(time_2_left)
-            st.write(calculation_1)
-            calculation_2 = str(calculation_1 - calculation_1)[1:]
-            min_cal = float(calculation_2) * 60
-            min_cal = trunc(min_cal)
-            calculation = str(time_2_left)
-            st.write(str(calculation_1), "hour and", str(min_cal), "minutes left")
-        else:
-            time_1_left = round(time_1_left, 2)
-            time_1_left = str(time_1_left)
-            st.write(time_1_left, "minutes left")
-    except:
-        pass
+    revs_left = float(doff_input) - float(revs_input)
+    time_1_left = revs_left / float(rpms_input)
+    if time_1_left > 60:
+        time_2_left = time_1_left / 60
+        calculation_1 = trunc(time_2_left)
+        calculation_2 = str(calculation_1 - calculation_1)[1:]
+        min_cal = float(calculation_2) * 60
+        min_cal = trunc(min_cal)
+        calculation = str(time_2_left)
+        st.write(str(calculation_1), "hour and", str(min_cal), "minutes left")
+    else:
+        time_1_left = round(time_1_left, 2)
+        time_1_left = str(time_1_left)
+        st.write(time_1_left, "minutes left")

5894e35185e59230f2332680647363bf4fab30fc
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 06:17:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ doff_input = st.number_input("Doff REVS", min_value=0, value=None, step=1, key="
 calculate_time = st.button("Calculate")
 
 if calculate_time:
-    revs_left = float(doff_input) - float(revs_input)
-    time_1_left = revs_left / float(rpms_input)
+    revs_left = doff_input - revs_input
+    time_1_left = revs_left / rpms_input
     if time_1_left > 60:
         time_2_left = time_1_left / 60
         st.write("Time left = " + str(time_2_left))

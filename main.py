9a8a48745cf39afbc31e274bc5299c2f4f173b72
AustinGInmanMills <xboxboy1387@gmail.com>
2024-10-11 07:20:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ if calculate_time:
         st.write("Time left = " + str(time_2_left))
     else:
         time_1_left = round(time_1_left, 2)
-        
+
         sec_cal = str(time_1_left-int(time_1_left))[1:]
-        st.write(sec_cal[1][:2])
+        st.write(sec_cal[1:])
 
         #seconds = sec_cal * 60
         #st.write(int(time_1_left),"m",str(seconds),"s")
